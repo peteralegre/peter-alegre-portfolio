b307bf401944f7b4c7c596c76317803a1b75bbec
Plofland <ploflan@gmail.com>
2024-09-24 13:44:17 -0400

    run dev edits

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7fc00317db2e3124"; }
+/******/ 		__webpack_require__.h = function() { return "a1afc56fa88a70db"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.c26edd41b80c87c2.hot-update.js
+++ .next/static/webpack/webpack.c26edd41b80c87c2.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e2fe72133a0ffff3"; }
+/******/ 	__webpack_require__.h = function() { return "a1afc56fa88a70db"; }
 /******/ }();
 /******/ 
 /******/ }
