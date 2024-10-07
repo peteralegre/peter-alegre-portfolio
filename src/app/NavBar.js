ef6da5a6be375f0134a3938939740b793a58731d
Peter Lofland <plofland@trhc.com>
2024-10-07 18:49:42 -0400

    add nextjs ui navbar, style it

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -1,13 +1,57 @@
-import Link from 'next/link';
+'use client';
+
 import React from 'react';
+import {
+	Navbar,
+	NavbarContent,
+	NavbarItem,
+	Link
+} from '@nextui-org/react';
+import styled from 'styled-components';
 
 const NavBar = () => {
 	return (
 		<>
-			<Link href="/">Work</Link>
-			<Link href="/about">About</Link>
+			<StyledNavbar shouldHideOnScroll>
+				<StyledNavbarContent>
+					<StyledNavbarItem>
+						<Link href="#">Work</Link>
+					</StyledNavbarItem>
+					<StyledNavbarItem isActive>
+						<Link href="/about">About</Link>
+					</StyledNavbarItem>
+					<StyledNavbarItem>
+						<Link href="#">Contact</Link>
+					</StyledNavbarItem>
+				</StyledNavbarContent>
+			</StyledNavbar>
 		</>
 	);
 };
 
 export default NavBar;
+
+const StyledNavbar = styled(Navbar)`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: #333; /* Example styling */
+	padding: 1rem;
+`;
+
+const StyledNavbarContent = styled(NavbarContent)`
+	display: flex;
+	gap: 1.5rem; /* Adjust spacing between items */
+`;
+
+const StyledNavbarItem = styled(NavbarItem)`
+	list-style-type: none;
+	a {
+		color: #fff; /* Example: change link color */
+		text-decoration: none;
+
+		&:hover {
+			color: #f0a500; /* Example: hover effect */
+		}
+	}
+`;
