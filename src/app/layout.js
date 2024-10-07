22ee91c089bcce492736826c5054e458ef8385ec
Peter Lofland <plofland@trhc.com>
2024-10-07 19:16:42 -0400

    add navbar to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,20 @@
 import { Inter } from 'next/font/google';
+import NavBar from './NavBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
 	title: 'Peter Alegre',
-	description: 'Photography Portfolio',
+	description: 'Photography Portfolio'
 };
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<NavBar />
+				<main>{children}</main>
+			</body>
 		</html>
 	);
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 import MasonryGrid from './MasonryGrid';
-import NavBar from './NavBar';
 
 export default function Home() {
 	return (
 		<main>
-			<NavBar/>
-			<MasonryGrid/>
+			<MasonryGrid />
 		</main>
 	);
 }
