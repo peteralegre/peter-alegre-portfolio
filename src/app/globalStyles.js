23ac10e8486a1e85f303e9275d4d3ab077bd18c9
Peter Lofland <plofland@trhc.com>
2024-10-07 20:59:35 -0400

    remove old globals, add styled-components globals

---
+++ src/app/globalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  /* Add your global styles here */
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+  }
+`;
+
+export default GlobalStyles;
+
