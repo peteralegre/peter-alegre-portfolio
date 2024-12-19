47b6d08474988e1ee3ea7e3a2bfa451fc53ee8f1
Plofland <ploflan@gmail.com>
2024-12-19 16:56:49 -0500

    add Peter name text to main

--- src/app/page.js
+++ src/app/page.js
@@ -5,30 +5,17 @@ import NavBar from './NavBar';
 export default function Home() {
 	return (
 		<StyledMain>
-			<StyledText>Peter Alegre</StyledText>
-			<NavBar />
 		</StyledMain>
 	);
 }
 
 const StyledMain = styled.main`
-	position: relative; /* Ensure main acts as the positioning context */
-	width: 100%; /* Adjust as needed */
-	height: 100vh; /* Adjust as needed */
+	width: 100%; 
+	height: 100vh; 
 	display: flex;
-	justify-content: right;
 	background-image: url('/photos/4 Street.jpg');
 	background-size: cover; 
 	background-position: center;
 	background-repeat: no-repeat;
 `;
 
-const StyledText = styled.div`
-	position: absolute;
-	top: 0;
-	left: 0;
-	padding: 20px;
-	color: #fff;
-	font-size: 2.6rem;
-	text-shadow: 5px 5px 5px #000;
-`;
