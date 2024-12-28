177ec5736c21f0a8fb9247b5040b5f240cf92f93
Plofland <ploflan@gmail.com>
2024-12-28 15:00:20 -0500

    remove extra navbar

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,14 +2,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
-import NavBar from '../NavBar';
 
 const About = () => {
 	return (
 		<AboutContainer>
-			<NavBarContainer>
-				<StyledNavBar />
-			</NavBarContainer>
 			<AboutContent>
 				<Headshot>
 					<Image
@@ -55,8 +51,8 @@ const AboutContainer = styled.div`
 
 const NavBarContainer = styled.div`
 	/* border: 2px solid blue; */
-    display: flex;
-    justify-content: flex-end;
+	display: flex;
+	justify-content: flex-end;
 `;
 
 const AboutContent = styled.div`
@@ -69,22 +65,6 @@ const AboutContent = styled.div`
 	@media screen and (max-width: 750px) {
 		width: 100vw;
 	}
-`;
-
-const StyledNavBar = styled(NavBar)`
-	position: absolute;
-	top: 16px;
-	right: 16px;
-	background-color: rgba(
-		255,
-		255,
-		255,
-		0.8
-	); /* Optional: Add a background for better visibility */
-	padding: 8px 16px; /* Optional: Add padding for a nicer look */
-	border-radius: 8px; /* Optional: Add rounded corners */
-	box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Optional: Add a subtle shadow */
-	z-index: 10; /* Ensure it appears above other elements */
 `;
 
 const Headshot = styled.div`
