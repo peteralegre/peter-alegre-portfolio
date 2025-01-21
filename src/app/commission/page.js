fed098a09acca928a231af4c3f01beb8a2a49ca0
Plofland <ploflan@gmail.com>
2025-01-20 20:08:36 -0500

    add responsive design commission photo grid

--- src/app/commission/page.js
+++ src/app/commission/page.js
@@ -1,13 +1,47 @@
 'use client';
+import { useEffect, useState } from 'react';
 import { MasonryPhotoAlbum } from 'react-photo-album';
 import 'react-photo-album/masonry.css';
 import styled from 'styled-components';
 import commission from './commissionPhotos';
 
 const CommissionWork = () => {
+	const [columnNumber, setColumnNumber] = useState(1);
+
+	useEffect(() => {
+		// Function to determine column numbers based on window width
+		const updateColumns = () => {
+			const width = window.innerWidth;
+			if (width >= 1248) {
+				setColumnNumber(4);
+			} else if (width >= 800) {
+				setColumnNumber(2);
+			} else {
+				setColumnNumber(1);
+			}
+		};
+
+		// Initial update
+		updateColumns();
+
+		// Event listener for resizing
+		window.addEventListener('resize', updateColumns);
+
+		// Cleanup event listener on component unmount
+		return () => {
+			window.removeEventListener(
+				'resize',
+				updateColumns
+			);
+		};
+	}, []);
+
 	return (
 		<StyledMason>
-			<MasonryPhotoAlbum photos={commission} columns={4}/>
+			<MasonryPhotoAlbum
+				photos={commission}
+				columns={columnNumber}
+			/>
 		</StyledMason>
 	);
 };
