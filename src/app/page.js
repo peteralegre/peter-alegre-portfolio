46f16bb3ce8f48f142613ccef005688dfe2ee8ca
Plofland <ploflan@gmail.com>
2025-03-22 11:49:50 -0400

    update mobile bg img, update conditonal styling

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,21 @@ import styled from 'styled-components';
 import Image from 'next/image';
 
 export default function Home() {
-	const [imageSrc, setImageSrc] = useState('/photos/4 Street.jpg');
-	const [imageSize, setImageSize] = useState({ width: 1200, height: 700 });
+	const [imageSrc, setImageSrc] = useState(
+		'/photos/4 Street.jpg'
+	);
+	const [imageSize, setImageSize] = useState({
+		width: 1200,
+		height: 700
+	});
 
 	useEffect(() => {
 		const updateImage = () => {
 			if (window.innerWidth < 800) {
-				setImageSrc('/personal/landscapeBirdsSky.jpg');
-				setImageSize({ width: 750, height: 1000 });
+				setImageSrc(
+					'/photos/clothingCollection.jpeg'
+				);
+				setImageSize({ width: 360, height: 543 });
 			} else {
 				setImageSrc('/photos/4 Street.jpg');
 				setImageSize({ width: 1200, height: 700 });
@@ -20,7 +27,11 @@ export default function Home() {
 
 		updateImage();
 		window.addEventListener('resize', updateImage);
-		return () => window.removeEventListener('resize', updateImage);
+		return () =>
+			window.removeEventListener(
+				'resize',
+				updateImage
+			);
 	}, []);
 
 	return (
@@ -37,10 +48,21 @@ export default function Home() {
 
 const StyledMain = styled.main`
 	display: flex;
+	width: 100%;
+	height: 100%; /* Set a fixed height for demonstration */
+	overflow-x: auto;
 `;
 
 const HeroImage = styled(Image)`
-	margin: auto;
-	box-shadow: 6px 5px 20px -4px rgba(0,0,0,0.75);
-	margin-top: 10px;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+
+  @media (min-width: 768px) { /* Applies styles for tablets and desktops */
+    width: auto;
+    height: auto;
+    margin: auto;
+    margin-top: 10px;
+    box-shadow: 6px 5px 20px -4px rgba(0, 0, 0, 0.75);
+  }
 `;
